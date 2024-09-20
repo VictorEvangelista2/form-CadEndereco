8ae9bbb0c6076c490aec38239cef50fd4afdc820
VICTOR EVANGELISTA CANDIDO DOS SANTOS <victor.evangelista@alunoescolassociais.org.br>
2024-09-20 08:05:41 -0300

    .

--- formCadEndereco.js
+++ formCadEndereco.js
@@ -16,4 +16,13 @@ const LimparFormulario = () =>{
 // Verifica o cep
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho do cep
-const cepValido = (cep) => cep.lenght == 8 && eNumero(cep);
+const cepValido = (cep) => cep.lenght == 8 && eNumero(cep);     
+
+
+// Função para preencher os campos relacionados ao cep
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro; //Coloca o valor de logradouro da API dentro do campo do formulario
+    document.getElementById('bairro').value = endereco.bairro;  // *
+    document.getElementById('cidade').value = endereco.localidade; // *
+    document.getElementById('uf').value = endereco.uf; // *
+}
